3454f75edc5bb69dcc2a176f01e2de8edcd94ad8
Vladimir Shits <rav9aa@gmail.com>
2021-11-13 14:53:12 +0300

    10-immutability, edit test

--- src/10-immutability/10.test.ts
+++ src/10-immutability/10.test.ts
@@ -15,9 +15,14 @@ test('upgrade laptop to Macbook', () => {
         }
     }
 
-    const userWithMac = upgradeUserToMackBook(user, 'MacBook')
+    const userWithMac1 = upgradeUserToMackBook(user, 'MacBook')
+    const userWithMac2 = upgradeUserToMackBook(user, 'no name laptop')
+    const userWithMac3 = {...user}
 
-    expect(userWithMac.laptop.title).toBe('MacBook')
-    expect(userWithMac.laptop.title).not.toBe(user.laptop.title)
+    expect(userWithMac1.laptop.title).toBe('MacBook')
+    expect(userWithMac1.laptop.title).not.toBe(user.laptop.title)
+    expect(userWithMac2).not.toBe(user)
+    expect(user).not.toBe(userWithMac3)
+    expect(user.laptop).toBe(userWithMac3.laptop)
 })
 
